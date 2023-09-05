641b2ea498a585106b41ea4aba5cd88aa75c856e
Jackson Henry <jackson.henry3@gmail.com>
2023-09-05 15:32:09 -0400

    remove cyclic permutations of volume profiles, change to library for partition generation

--- src/volume_profiles.rs
+++ src/volume_profiles.rs
@@ -1,29 +1,69 @@
-fn main() {
-    let volume = 64;
-    println!("{:?}", volume_profiles(volume));
+use std::{collections::HashSet, hash::Hash};
+
+use integer_partitions::Partitions;
+use itertools::Itertools;
+
+//derive eq
+#[derive(Debug, Clone)]
+pub struct volume_profile {
+    profile: Vec<usize>,
+    volume: usize,
+    product: usize,
+    id: usize,
 }
 
-fn volume_profiles(volume: u32) -> Vec<Vec<u32>> {
-    /// volume profile is a list of spatial slice sizes
-    /// But VOLUME is the number of triangles
-    ///  2 * Total(Volume_profile) = VOLUME
+impl volume_profile {
+    pub fn new(profile: Vec<usize>) -> volume_profile {
+        let volume = profile.iter().fold(0, |acc, x| acc + x);
+        let product = profile.iter().fold(1, |acc, x| acc * x);
 
-    assert!(volume > 0, "Volume must be greater than 0");
-    assert!((volume % 2) == 0, "Volume must be even");
-
-    let mut volume_profile: Vec<Vec<u32>> = Vec::new();
-    let total_length = volume / 2u32;
-
-    volume_profile.push(vec![total_length]);
-
-    // find all combinations of integers that add to total_length exluding 0
-
-    for value in 1..total_length {
-        for smaller_volume_profile in volume_profiles(2 * (total_length - value)) {
-            let mut new_volume_profile = smaller_volume_profile;
-            new_volume_profile.push(value);
-            volume_profile.push(new_volume_profile);
+        let total = profile.iter().fold(1, |acc, x| acc * x);
+        let mut id = 0;
+        for (index, val) in profile.iter().enumerate() {
+            let next = profile[(index + 1) % profile.len()];
+            id += val * (total / next);
+        }
+        volume_profile {
+            profile,
+            volume,
+            product,
+            id,
         }
     }
-    volume_profile
+}
+
+impl PartialEq for volume_profile {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+impl Eq for volume_profile {}
+
+impl Hash for volume_profile {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
+    }
+}
+
+pub fn non_cyclic_permutations(vec: Vec<usize>) -> HashSet<volume_profile> {
+    let mut result: HashSet<_> = vec
+        .iter()
+        .cloned()
+        .permutations(vec.len())
+        .map(|x| volume_profile::new(x))
+        .collect();
+
+    result
+}
+
+pub fn volume_profiles(volume: usize) -> impl Iterator<Item = HashSet<volume_profile>> {
+    //assert div 2, with message
+    assert_eq!(volume % 2, 0, "volume must be divisible by 2");
+
+    let mut a = Partitions::new(volume / 2);
+
+    std::iter::from_fn(move || match a.next() {
+        Some(profile) => Some(non_cyclic_permutations(profile.to_vec())),
+        _ => None,
+    })
 }
