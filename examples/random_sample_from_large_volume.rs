e1226e1b249ed181949c48c5c21918936b71536b
Jackson Henry <jackson.henry3@gmail.com>
2023-10-24 10:16:59 -0400

    push before pull

--- examples/random_sample_from_large_volume.rs
+++ examples/random_sample_from_large_volume.rs
@@ -1,3 +1,10 @@
-use cdt_rust;
+// run this with cargo r -r --example random_sample_from_large_volume
+use cdt_rust::{self, utils, volume_profiles::weighted_random_partition};
 
-fn main() {}
+fn main() {
+    let a: Vec<_> = (0..10_000)
+        .map(|_| weighted_random_partition(4, 10))
+        .collect();
+
+    fcv
+}
