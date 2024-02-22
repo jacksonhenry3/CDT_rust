1ce49f64228151afc1517bd274919fe29e006916
Jackson Henry <jackson.henry3@gmail.com>
2024-02-22 17:51:28 -0500

    testing script

--- examples/testing.rs
+++ examples/testing.rs
@@ -8,19 +8,18 @@ use std::fs::File;
 use std::io::Write;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
+
 fn main() {
     let mut rng = rand::thread_rng();
-    for _ in 0..10 {
-        // let v: Vec<_> = (0..5).map(|_| rng.gen_range(3..41)).collect();
-        let v = vec![3, 29, 22, 21, 3];
+    for _ in 0..1 {
+        let v: Vec<_> = (0..5).map(|_| rng.gen_range(3..41)).collect();
+        // let v = vec![6,10, 7, 4, 6];
 
         let cdt = cdt::CDT::random(&VolumeProfile::new(v.clone()));
-        println!("--");
+
         let s = cdt_rust::eh_action(&cdt);
-        if s.abs() > 0.000001 {
-            println!("{:?}", v);
-            println!("{:?} {}", cdt.volume_profile(), s.abs());
-            println!()
-        }
+        // if s.abs() > 0.000001 {
+        println!("{}", s);
+        // }
     }
 }
